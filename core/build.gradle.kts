f09d4bd43b20f383468a35c6a5d9179032bb2228
sjkyomg <sjkyomg@easyshop.co.kr>
2025-08-01 12:15:30 +0900

    feat: 타임리프 dependencies 추가

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("com.github.codemonstur:embedded-redis:1.0.0")
 }
