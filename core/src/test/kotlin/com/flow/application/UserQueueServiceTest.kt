a7dd4fec9b5bf3a9d69ac2325ca221d888c73726
sjkyomg <sjkyomg@easyshop.co.kr>
2025-08-01 10:01:04 +0900

    feat: 서비스 통합 테스트 추가

--- core/src/test/kotlin/com/flow/application/UserQueueServiceTest.kt
+++ core/src/test/kotlin/com/flow/application/UserQueueServiceTest.kt
@@ -1,11 +1,15 @@
 package com.flow.application
 
 import com.flow.EmbeddedRedis
+import com.flow.support.ApplicationException
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Import
+import org.springframework.data.redis.connection.ReactiveRedisConnection
+import org.springframework.data.redis.core.ReactiveRedisTemplate
 import org.springframework.test.context.ActiveProfiles
 import reactor.test.StepVerifier
 
@@ -14,43 +18,101 @@ import reactor.test.StepVerifier
 @ActiveProfiles("test")
 class UserQueueServiceTest @Autowired constructor(
     private val userQueueService: UserQueueService,
-    private val userQueueRepository: UserQueueRepository
+    private val userQueueRepository: UserQueueRepository,
+    private val reactiveRedisTemplate: ReactiveRedisTemplate<String, String>
 ) {
+
+    @BeforeEach
+    fun beforeEach() {
+        val redisConnection: ReactiveRedisConnection = reactiveRedisTemplate.connectionFactory.reactiveConnection
+        StepVerifier.create(redisConnection.serverCommands().flushAll())
+            .expectNextCount(1)
+            .verifyComplete()
+    }
+
+
 
     @Test
     fun registerWaitQueue() {
-        StepVerifier.create(userQueueService.registerWaitQueue("default", 1))
-            .expectNext(5L)
+        StepVerifier.create(userQueueService.registerWaitQueue("default", 100))
+            .expectNext(1L)
             .verifyComplete()
 
-        StepVerifier.create(userQueueService.registerWaitQueue("default", 2))
+        StepVerifier.create(userQueueService.registerWaitQueue("default", 101))
+            .expectNext(2L)
+            .verifyComplete()
+
+        StepVerifier.create(userQueueService.registerWaitQueue("default", 102))
             .expectNext(3L)
-            .verifyComplete()
-
-        StepVerifier.create(userQueueService.registerWaitQueue("default", 3))
-            .expectNext(4L)
             .verifyComplete()
     }
 
     @Test
     fun alreadyRegisterWaitQueue(){
-        StepVerifier.create(userQueueService.registerWaitQueue("default", 1))
-            .expectNext(5L)
+        StepVerifier.create(userQueueService.registerWaitQueue("default", 100))
+            .expectNext(1L)
             .verifyComplete()
 
-        StepVerifier.create(userQueueService.registerWaitQueue("default", 1))
-            .expectErrorMatches { it.message == "이미 대기열에 등록된 유저입니다." }
+        StepVerifier.create(userQueueService.registerWaitQueue("default", 100))
+            .expectError(ApplicationException::class.java)
             .verify()
     }
 
     @Test
-    fun allowUser() {
+    fun emptyAllowUser() {
+        StepVerifier.create(userQueueService.allowUser("default", 3))
+            .expectNext(0L)
+            .verifyComplete()
+    }
 
+
+    @Test
+    fun allowUser() {
+        StepVerifier.create(userQueueService.registerWaitQueue("default", 100)
+                .then(userQueueService.registerWaitQueue("default", 101))
+                .then(userQueueService.registerWaitQueue("default", 102))
+                .then(userQueueService.allowUser("default", 5)))
+            .expectNext(3L)
+            .verifyComplete()
+    }
+
+
+    @Test
+    fun allowUserAfterRegisterWaitQueue() {
+        StepVerifier.create(userQueueService.registerWaitQueue("default", 100)
+            .then(userQueueService.registerWaitQueue("default", 101))
+            .then(userQueueService.registerWaitQueue("default", 102))
+            .then(userQueueService.allowUser("default", 3))
+            .then(userQueueService.registerWaitQueue("default", 200)))
+            .expectNext(1L)
+            .verifyComplete()
+    }
+
+
+    @Test
+    fun isNotAllowed() {
+        StepVerifier.create(userQueueService.isAllowed("default", 100))
+            .expectNext(false)
+            .verifyComplete()
+    }
+
+
+    @Test
+    fun allowAfterNotAllowed() {
+        StepVerifier.create(userQueueService.registerWaitQueue("default", 100)
+            .then(userQueueService.allowUser("default", 1))
+            .then(userQueueService.isAllowed("default", 101)))
+            .expectNext(false)
+            .verifyComplete()
     }
 
     @Test
     fun isAllowed() {
-
+        StepVerifier.create(userQueueService.registerWaitQueue("default", 100)
+            .then(userQueueService.allowUser("default", 1))
+            .then(userQueueService.isAllowed("default", 100)))
+            .expectNext(true)
+            .verifyComplete()
     }
 
 }
