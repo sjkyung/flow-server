b9fd4d62c784abdb51aceff247e0a85492b4586b
sjkyomg <sjkyomg@easyshop.co.kr>
2025-08-01 15:42:39 +0900

    feat: 스케줄러 설정 추가

--- core/src/main/kotlin/com/flow/FlowApplication.kt
+++ core/src/main/kotlin/com/flow/FlowApplication.kt
@@ -2,8 +2,10 @@ package com.flow
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class FlowApplication
 
 fun main(args: Array<String>) {
