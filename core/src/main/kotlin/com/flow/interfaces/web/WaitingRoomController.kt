d8bf0f29089ff973cd7f15d174abba6402cf2e48
sjkyomg <sjkyomg@easyshop.co.kr>
2025-08-01 12:13:09 +0900

    feat: 대기열 페이지 추가

---
+++ core/src/main/kotlin/com/flow/interfaces/web/WaitingRoomController.kt
@@ -0,0 +1,40 @@
+package com.flow.interfaces.web
+
+import com.flow.application.UserQueueService
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.reactive.result.view.Rendering
+import reactor.core.publisher.Mono
+
+@Controller
+class WaitingRoomController(
+    private val userQueueService: UserQueueService
+) {
+
+
+    @GetMapping("/waiting-room")
+    fun waitingRoomPage(
+        @RequestParam(name = "queue", defaultValue = "default") queue: String,
+        @RequestParam(name = "user_id") userId: Long,
+        @RequestParam(name = "redirect_url") redirectUrl: String,
+    ): Mono<Rendering>{
+
+        //대기가 있는지 확인
+        //없으면 타겟 페이지
+        //있으면 대기 조회
+        return userQueueService.isAllowed(queue, userId)
+            .filter { allowed -> allowed }
+            .flatMap { Mono.just(Rendering.redirectTo(redirectUrl).build()) }
+            .switchIfEmpty(
+                userQueueService.registerWaitQueue(queue, userId)
+                .onErrorResume { userQueueService.getRank(queue, userId)}
+                .map { rank -> Rendering.view("waiting-room.html")
+                    .modelAttribute("rank", rank)
+                    .modelAttribute("userId", userId)
+                    .modelAttribute("queue", queue)
+                    .build()
+                }
+            )
+    }
+}
